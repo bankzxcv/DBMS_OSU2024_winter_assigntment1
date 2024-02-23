e83e9c7b5f2bc53eca3d2c78ddac5fde58f97e4f
porschelook <33505600+porschelook@users.noreply.github.com>
2024-02-22 21:42:54 -0800

    main

--- assignment_3/main.cpp
+++ assignment_3/main.cpp
@@ -15,26 +15,41 @@ Skeleton code for linear hash indexing
 #include "classes.h"
 using namespace std;
 
-int main(int argc, char* const argv[]) {
+int main(int argc, char *const argv[])
+{
   // Create the index
-  LinearHashIndex emp_index("EmployeeIndex");
-  emp_index.createFromFile("Employee.csv");
 
   // Create the EmployeeRelation file from Employee.csv
   // StorageBufferManager manager("EmployeeRelation");
   // manager.createFromFile("Employee.csv");
 
-  // Loop to lookup IDs until user is ready to quit
-  // int id;
-  // cout << "Enter employee ID to search: ";
-  // cin >> id;
-  // Record result = emp_index.findRecordById(id);
-  // if (result.id != -1) {
-  //     cout << "Record found:\n";
-  //     result.print();
-  // } else {
-  //     cout << "Record not found.\n";
-  // }
+  LinearHashIndex emp_index("EmployeeIndex");
 
+  emp_index.createFromFile("Employee.csv");
+  // Loop to lookup IDs until user is ready to quit
+  while (true)
+  {
+    try
+    {
+      /* code */
+
+      int id;
+      cout << "Enter employee ID to search: (-1) Exit : ";
+      cin >> id;
+      emp_index.findRecordById(id);
+      if (id == -1)
+      {
+        break;
+      }
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+  }
   return 0;
 }
